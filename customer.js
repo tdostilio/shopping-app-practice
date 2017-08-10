6994a3c9dbccda00f09092f22f8ac25ddf4ef113
tdostilio <tdostilio@gmail.com>
2017-08-10 11:39:39 -0400

    adds ability to save customer to database

--- customer.js
+++ customer.js
@@ -1,4 +1,34 @@
 require('dotenv').config();
+const pg = require('pg-promise')();
+const dbConfig = {
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+};
+
+class Customer {
+    constructor(name,email,address,password) {
+        this.db = pg(dbConfig);
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.password = password;
+    }
+    save() {
+        return this.db.query(`
+        insert into customers 
+        (name, email, address, password)
+        values
+        ('${this.name}','${this.email}','${this.address}','${this.password}');
+        `);
+    }
+};
+
+module.exports = Customer;
+
+
+
 
 
 // var db = require('db')
--- test/test-customer-model.js
+++ test/test-customer-model.js
@@ -1,7 +1,16 @@
 const expect = require('chai').expect;
+const Customer = require('../customer');
 
-describe('customers', () => {
-    it('should be able to save to the database', () => {
-        expect(1 + 1).to.equal(2);
+
+
+describe('Customers', () => {
+    it('should be able to save to the database', (done) => {
+        
+        let myCustomer = new Customer('me', 'me@me.com', '123 me street', 'm3m3m3');
+        myCustomer
+            .save()
+            .then((result) => {
+                done();
+            });
     });
 });
